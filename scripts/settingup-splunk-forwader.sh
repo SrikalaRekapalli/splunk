02750351c8b22f56c4bea9934e40c3f627a337e7
SrikalaRekapalli <srekapalli@sysgain.com>
2016-12-14 10:09:09 +0530

    Add files via upload

---
+++ scripts/settingup-splunk-forwader.sh
@@ -0,0 +1,35 @@
+# This script will setup the splunk forwarder 
+
+#ip address of splunk server
+splunkserverip=$1
+logmonitorfolder=$2
+clientvmname=$3
+
+#splunk package folder
+
+mkdir /home/$clientvmname/splunkpkg
+cd /home/$clientvmname/splunkpkg
+
+#dowload the latest splunk package 
+
+wget -O splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=universalforwarder&filename=splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz&wget=true' 
+
+#extract the files from zip folder
+tar xvzf splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz
+sleep 30
+#move the splunk to opt folder
+mv /home/$clientvmname/splunkpkg/splunkforwarder /opt
+
+cd /opt/splunkforwarder/bin
+
+./splunk start --accept-license
+
+/opt/splunkforwarder/bin/splunk add forward-server $splunkserverip:9997
+
+./splunk add monitor $logmonitorfolder
+
+cd /opt/splunkforwarder/bin
+
+./splunk restart
+
+
