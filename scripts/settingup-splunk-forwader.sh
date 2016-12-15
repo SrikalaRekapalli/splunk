3b08f6022206be65d207c3d741843abed7e0a8b9
SrikalaRekapalli <srekapalli@sysgain.com>
2016-12-15 14:56:24 +0530

    Update settingup-splunk-forwader.sh

--- scripts/settingup-splunk-forwader.sh
+++ scripts/settingup-splunk-forwader.sh
@@ -1,21 +1,21 @@
 ######################################################################################################
-#	File         : settingup-splunk-forwader.sh
-#	Description  : This script will install splunk universal forwader
+#    File         : settingup-splunk-forwader.sh
+#    Description  : This script will install splunk universal forwader
 #   Usage        : sh filename.sh splunkserverip logmonitorfolder clientvmname
 ######################################################################################################
 
 #!/bin/bash
 
-clientvmname=$1
+clientvmusername=$1
 
 #splunk package folder
 
-mkdir /home/$clientvmname/splunkpkg
-cd /home/$clientvmname/splunkpkg
+mkdir /home/$clientvmusername/splunkpkg
+cd /home/$clientvmusername/splunkpkg
 
-#dowload the latest splunk package 
+#dowload the latest splunk package
 
-wget -O splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=universalforwarder&filename=splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz&wget=true' 
+wget -O splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=universalforwarder&filename=splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz&wget=true'
 
 sleep 40
 #extract the files from zip folder
@@ -23,13 +23,12 @@ tar xvzf splunkforwarder-6.5.1-f74036626f0c-Linux-x86_64.tgz
 
 
 #move the splunk to opt folder
-mv /home/$clientvmname/splunkpkg/splunkforwarder /opt/
+mv /home/$clientvmusername/splunkpkg/splunkforwarder /opt/
 
-cd /opt/splunkforwarder/bin
+#cd /opt/splunkforwarder/bin
 
-./splunk start 
+#./splunk start
 
 #/opt/splunkforwarder/bin/splunk add forward-server $splunkserverip:9997
 
 #./splunk add monitor $logmonitorfolder
-
